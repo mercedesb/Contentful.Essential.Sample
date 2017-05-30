4126fed67a3828b34ca1298aa082f7c93ef0f27b
mercedesb <mercedesbernard@live.com>
2017-05-29 20:55:26 -0500

    home logic

--- Contentful.Essential.Sample/Controllers/HomeController.cs
+++ Contentful.Essential.Sample/Controllers/HomeController.cs
@@ -1,30 +1,70 @@
-﻿using System;
+﻿using Contentful.Core;
+using Contentful.Essential.Models;
+using Contentful.Essential.Models.Configuration;
+using Contentful.Essential.Sample.Models;
+using Contentful.Essential.Sample.Models.ViewModels;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Contentful.Essential.Sample.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        protected readonly IContentRepository<Pattern> _repo;
+        protected readonly IContentDeliveryClient _client;
+        protected readonly IContentfulOptions _config;
+
+        public HomeController(IContentRepository<Pattern> repo, IContentDeliveryClient client, IContentfulOptions config)
         {
-            return View();
+            _repo = repo;
+            _client = client;
+            _config = config;
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> Index()
         {
-            ViewBag.Message = "Your application description page.";
+            HomeViewModel model = new HomeViewModel();
 
-            return View();
+            IEnumerable<Pattern> patterns = await _repo.GetAll();
+
+            if (patterns == null)
+                return View(model);
+
+            model.Patterns = patterns;
+
+            return View(model);
         }
 
-        public ActionResult Contact()
+        public async Task<ActionResult> IndexTwo()
         {
-            ViewBag.Message = "Your contact page.";
+            HomeViewModel model = new HomeViewModel();
 
-            return View();
+            IEnumerable<Pattern> patterns = await _client.Instance.GetEntriesByTypeAsync<Pattern>("Pattern");
+
+            if (patterns == null)
+                return View(model);
+
+            model.Patterns = patterns;
+
+            return View("Index", model);
+        }
+
+        public async Task<ActionResult> IndexThree()
+        {
+            HomeViewModel model = new HomeViewModel();
+
+            var httpClient = new HttpClient();
+            var client = new ContentfulClient(httpClient, _config.DeliveryAPIKey, _config.SpaceID);
+            IEnumerable<Pattern> patterns = await client.GetEntriesByTypeAsync<Pattern>("Pattern");
+
+            if (patterns == null)
+                return View(model);
+
+            model.Patterns = patterns;
+
+            return View("Index", model);
         }
     }
 }
---
+++ Contentful.Essential.Sample/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Contentful.Essential.Sample.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pattern> Patterns { get; set; }
+    }
+}
